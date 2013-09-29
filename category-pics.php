52cf0c471ba4db4960500275fbdb572daeb4ef4e
Florian Posdziech <florian@posdziech.de>
2013-09-29 13:27:39 +0200

    Remove more unnecessary stuff and add some
    * Add thumbnail image to post header
    * adjust margins and padding

--- category-pics.php
+++ category-pics.php
@@ -17,7 +17,7 @@ get_header(); ?>
 					Bilderarchiv
 				</h2>
 			</header><!-- end .page-header -->
-
+    
 			<?php rewind_posts(); ?>
 
 			<?php /* Start the Loop */ ?>
@@ -25,18 +25,15 @@ get_header(); ?>
 
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-                <aside class="entry-details">
-                    <ul class="clearfix">
-                        <li class="entry-date"><a href="<?php the_permalink(); ?>"><?php echo get_the_date(); ?></a></li>
-                        <li class="entry-comments"><?php comments_popup_link( __( '0 comments', 'waipoua' ), __( '1 comment', 'waipoua' ), __( '% comments', 'waipoua' ), 'comments-link', __( 'comments off', 'waipoua' ) ); ?></li>
-                        <li class="entry-edit"><?php edit_post_link(__( 'Edit Post &rarr;', 'waipoua') ); ?></li>
-                    </ul>
-                </aside><!--end .entry-details -->
-
                 <header class="entry-header">
+                    <?php if ( has_post_thumbnail() ): ?>
+                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+                    <?php endif; ?>
                     <h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'waipoua' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
                 </header><!--end .entry-header -->
 
+                <div class="entry-content clearfix">
+                </div><!-- end .entry-content -->
 
             </article><!-- end post -<?php the_ID(); ?> -->
 			<?php endwhile; // end of the loop. ?>
